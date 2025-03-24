81912f14f03b1ab95793bb5a9ba406533d1f072b
ruannbraz <ruanbraz76@gmail.com>
2025-03-24 16:15:36 -0300

    exercicio resolvido

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar() {
+        var msg = window.document.getElementById("msg")
+        var img = window.document.getElementById('imagem')
+        var data = new Date()
+        var hora = data.getHours()
+        msg.innerHTML = `Agora são ${hora} horas.`
+        if (hora > 0 && hora < 12) {
+            img.src = 'fotomanha.png'
+            document.body.style.background = '#e2cd9f'
+           //BOM DIA 
+        } else if (hora >= 12 && hora < 18) {
+            //BOA TARDE
+            img.src = 'fototarde.jpg'
+            document.body.style.background = '#b9846f'
+        } else {
+            //BOA NOITE
+            img.src = 'fotonoite.jpg'
+            document.body.style.background = '#515154'
+        }
+}
